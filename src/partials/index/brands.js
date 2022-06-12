e4e8f2717ed1795af171d6f31d73ebd943eebd23
Volodymyr Doskochinskiy <doskovol1@gmail.com>
2022-06-12 19:04:50 -0300

    Index page adaptive is completed

--- src/partials/index/brands.js
+++ src/partials/index/brands.js
@@ -2,11 +2,20 @@ import Swiper, { Navigation, Autoplay } from "swiper"
 
 new Swiper(".brands__slider", {
   modules: [Navigation, Autoplay],
-  slidesPerView: 4,
+  slidesPerView: 2,
   loop: true,
   autoplay: { delay: 5000 },
   navigation: {
     nextEl: ".brands__nav--next",
     prevEl: ".brands__nav--prev"
+  },
+  breakpoints: {
+    601: {
+      slidesPerView: 3
+    },
+
+    769: {
+      slidesPerView: 4
+    } 
   }
 })
